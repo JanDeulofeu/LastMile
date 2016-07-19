e7da9ed3fa490f5edc81e5e41d976ba14bcad45e
jan <jan.deulofeu@scalarmonitoring.com>
2016-07-19 21:14:07 +0100

    Test: Adding rest client

--- src/test/java/com/lastmile/rest/RestControllerTest.java
+++ src/test/java/com/lastmile/rest/RestControllerTest.java
@@ -1,15 +1,19 @@
 package com.lastmile.rest;
 
 import com.lastmile.ConfigurationApp;
-import com.lastmile.model.Ant;
-import com.sun.tools.javac.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by jan on 19/07/2016.
@@ -25,11 +29,31 @@ public class RestControllerTest {
     RestTemplate restTemplate = new RestTemplate();
 
     @Test
-    public void getAntsRestTest()
-    {
+    public void getAntsRestTest() {
 
-        List<Ant> ants = (List)restTemplate.getForObject(HOST,List.class);
 
-        System.out.println(ants);
+        ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity(HOST, Object[].class);
+        Object[] objects = responseEntity.getBody();
+
+        assertNotNull(objects);
+        assertTrue(objects.length > 0);
     }
+
+    @Test
+    public void postAntsDamageTest() {
+
+        ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity(HOST, Object[].class);
+        Object[] objectsInit = responseEntity.getBody();
+
+        HttpEntity<String> request = new HttpEntity<>("");
+
+        restTemplate.postForObject(HOST, request, String.class);
+
+        responseEntity = restTemplate.getForEntity(HOST, Object[].class);
+        Object[] objectsEnd = responseEntity.getBody();
+
+
+        assertNotEquals(objectsInit,objectsEnd );
+    }
+
 }
